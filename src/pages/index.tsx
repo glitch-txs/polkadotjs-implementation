71bbf7e69fa2862bd1ab2f7410ea6a6f1c521b81
Glitch <glitch.txs@gmail.com>
2023-04-20 01:35:03 -0300

    boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
+      <button className='py-0.5 px-2 rounded-md hover:bg-gray-800 transition duration-75 border-2' >
+        Connect
+      </button>
     </main>
   )
 }
